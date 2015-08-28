37df734ff6bf35dd5317c050b28eda5721bba81b
Konsta 'sooda' Hölttä <sooda@iki.fi>
2015-08-28 22:04:05 +0300

    some sendMessage error handling
    random 502s, and 400s because i don't know

--- tgbot.py
+++ tgbot.py
@@ -9,18 +9,33 @@ class TgbotConnection:
         return 'https://api.telegram.org/bot{}/{}'.format(self.token, method)
 
     def makeRequest(self, reqname, **params):
-        logging.debug('>>> {}: {}'.format(reqname, params))
-        response = requests.get(self.apiurl(reqname), params=params)
-        response.encoding = 'utf-8'
-        # version mismatches in our installs
-        try:
-            json = response.json()
-        except TypeError:
-            json = response.json
-        logging.debug('<<< {}'.format(json))
-        if not json['ok']:
-            raise RuntimeError('Bad request, response: {}'.format(json))
-        return json['result']
+        while True:
+            logging.debug('>>> {}: {}'.format(reqname, params))
+            response = requests.get(self.apiurl(reqname), params=params)
+
+            response.encoding = 'utf-8'
+            # version mismatches in our installs
+            try:
+                json = response.json()
+            except TypeError:
+                json = response.json
+            logging.debug('<<< {}'.format(json))
+
+            # error 502 happens sometimes
+            if json is None:
+                continue
+
+            if not json['ok']:
+                if json.get('description') == 'Error: PEER_ID_INVALID':
+                    # happens for sendMessage sometimes. FIXME: what makes the peer invalid?
+                    # return value can be ignored here for now
+                    logging.error('FIXME: what is this?')
+                    return
+                if json.get('description') == 'Error: Bot was kicked from a chat':
+                    logging.warning('FIXME: handle this somehow?')
+                    return
+                raise RuntimeError('Bad request, response: {}'.format(json))
+            return json['result']
 
     def getMe(self):
         return self.makeRequest('getMe')
